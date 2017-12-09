9cd6c9cbc9aa07344c525f54e7100afba7e90622
Rp7rf <westhouse51@gmail.com>
2017-12-09 17:58:12 +0900

    ジャイロセンサーをpostするデータを追加

--- gyro/js/index.js
+++ gyro/js/index.js
@@ -3,7 +3,6 @@
   var $zo;
 
   $(function () {
-    // $zo = $("#zo");
     window.addEventListener("deviceorientation", deviceorientationHandler);
   });
 
@@ -20,28 +19,22 @@
     // Z軸
     var alpha = event.alpha;
     var html = "";
-    html += "X回転 : " + beta + "<br>";
-    html += "Y回転 : " + gamma + "<br>";
-    html += 'Z回転 : ' + alpha;
+
+    gyro_obj = {
+      beta : beta,
+      gamma : gamma,
+      alpha : alpha
+    }
+
     var canvas = document.getElementById("sample");
     var ctx = canvas.getContext("2d");
     ctx.fillText(html, 20, 20);
-    // $("#debug").html(html);
-    /*
-    function main() {
-        // canvas要素取得
-        var canvas = document.getElementById("sample");
 
-        // 描画コンテキスト取得
-        var ctx = canvas.getContext("2d");
-
-        // 文字列を描画
-        ctx.fillText("Hello World", 20, 20);
-    } */
-
-    // $zo.css({
-    //   "-webkit-transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)",
-    //   "transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)"
-    // })
+    var json_upload = "gyro=" + JSON.stringify(gyro_obj);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", "https://genga-rp7rf.c9users.io");
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send(json_upload);
+    console.log('sended')
   }
 })();
